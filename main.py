c44a72e29ab229460ac767ad94567a54d0d43240
Nazar <nazarhuzner@gmail.com>
2022-10-27 17:21:15 +0300

    Animals

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ class home_Animals:
         self.hignrun = False
         self.fly = False
         self.highjump = False
+class Dog(home_Animals):
+    def __init__(self):
+        super().__init__()
+        self.hignrun = True
+        self.hignjump = False
+        self.fly = False
+class Parrot(home_Animals):
+    def __init__(self):
+        super().__init__()
+        self.hignrun = False
+        self.hignjump = False
+        self.fly = True
+class Cat(home_Animals):
+    def __init__(self):
+        super().__init__()
+        self.hignrun = False
+        self.hignjump = True
+        self.fly = False
